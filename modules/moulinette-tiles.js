f44b938fb09f872be289df73de754add4b786397
Sven Werlen <sven.werlen@pm.me>
2021-05-23 23:08:04 -0400

    Fix missing sas

--- modules/moulinette-tiles.js
+++ modules/moulinette-tiles.js
@@ -49,7 +49,7 @@ export class MoulinetteTiles extends game.moulinette.applications.MoulinetteForg
     r.sas = pack.isRemote && game.moulinette.user.sas ? "?" + game.moulinette.user.sas : ""
     r.assetURL = r.filename.match(/^https?:\/\//) ? r.filename : `${URL}${pack.path}/${r.filename}`
     if(r.filename.endsWith(".webm")) {
-      const thumbnailURL = showThumbs ? r.assetURL.substr(0, r.assetURL.lastIndexOf('.') + 1) + "webp" : ""
+      const thumbnailURL = showThumbs ? r.assetURL.substr(0, r.assetURL.lastIndexOf('.') + 1) + "webp" + r.sas : ""
       return `<div class="tileres video draggable fallback" title="${r.filename}" data-idx="${idx}">` +
         `<img width="100" class="cc_image" height="100" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" style="background-image: url(${thumbnailURL})"/>` +
         `<video width="100" height="100" autoplay loop muted><source src="" data-src="${r.assetURL}${r.sas}" type="video/webm"></video></div>`
